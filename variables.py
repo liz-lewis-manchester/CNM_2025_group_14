48b9b7537e228685b63695f6c8295e4b649e1e23
Daisy Waite <daisy.waite-2@student.manchester.ac.uk>
2025-12-15 16:06:15 +0000

    Update variables.py
    Shorter version of variables.py
    
    Specifies variable conditions

--- variables.py
+++ variables.py
@@ -1,55 +1,21 @@
-#Code to declare variables
+#Declaring the variables of the system
+#User-defined model inputs.
+#Designed for the user to edit the values below to change the simulation.
 
-import numpy as np
-import pandas as pd
 
-#Initialise model demain and variables
-def variables():
-  L = 20.0    #domain length (m)
-  U = 0.1     #velocity (m/s)
-  T = 300     #domain time(s)
-  dx = 0.2    #spacial resolution (m)
-  dt = 10     #timestep (s)
-  return(L, U, T, dx, dt)
-L, U, T, dx, dt = variables()
+#Spatial and temporal domain
+L = 20.0        #Length of model in metres
+T = 300.0       #Length of test in seconds
 
-#Number of discrete intervals in space and time
-Nx = int(L/dx) + 1
-Nt = int(T/dt)
-x = np.linspace(0, L, Nx)
+#Spatial and temporal resolution
+dx = 0.2        #in metres
+dt = 10.0       #in seconds
 
-#Theta is the pollutant concentration
-#An array to hold the current solution
-theta_new = np.zeros(Nx)
+#Velocity of the stream
+u = 0.1         #in metres per second
 
-#An array to store the previous solution
-theta_old = np.zeros(Nx)
-
-#Arrays to hold the martrix coefficients
-A = np.zeros(Nx-1)
-B= np.zeros(Nx-1)
-
-#Right hand side array
-F = np.zeros(Nx-1)
-
-#Check if u = constant or u = u(x) and define array
-if np.isscalar(U):
-  u_array = np.full(Nx -1, U)
-
-else:
-  u_array = np.array(U)
-  if len(u_array) != Nx-1:
-    raise ValueError("Velocity array U(x) must be a list or array with Nx-1 values")
-
-#Values of A and B based on U, dx and dt
-A[:] = u_array * dt/dx
-B[:] = 1 - A[:]
-
-#Read in initial condictions 
-df = pd.read_csv("initial_conditions.csv", encoding="latin1")
-x_data = df.iloc[:, 0].to_numpy(float)
-c_data = df.iloc[:, 1].to_numpy(float)
-theta_old = np.interp(x, x_data, c_data, left=c_data[0], right=c_data[-1])
+#Initial pollutant concentration at x = 0 - boundary condition
+theta_source = 250.0  #in micro-grams per metre cubed
 
 
 
